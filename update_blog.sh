789b1d2322dcb858bd9302dd4cfcd0c6c9f12b01
MrRaph_ <MrRaph@users.noreply.github.com>
2017-02-02 16:18:24 +0100

    Update update_blog.sh

--- update_blog.sh
+++ update_blog.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker service update --force --container-label-add pushed_at="$REPOSITORY_PUSHED_AT" techan-${REPOSITORY_TAG}
+docker service update --force --container-label-add pushed_at="$REPOSITORY_PUSHED_AT" --image=mrraph/blog:${REPOSITORY_TAG} techan-${REPOSITORY_TAG}
